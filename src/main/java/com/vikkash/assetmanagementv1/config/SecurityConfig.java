86a279468866f264e55254ba24c4ad750d8a81eb
Vikkash <brokzvicky@gmail.com>
2026-06-28 16:35:41 +0530

    domain updates

--- src/main/java/com/vikkash/assetmanagementv1/config/SecurityConfig.java
+++ src/main/java/com/vikkash/assetmanagementv1/config/SecurityConfig.java
@@ -63,6 +63,8 @@ public class SecurityConfig {
         cfg.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "https://haodaasset.netlify.app"
+                "https://haodaasset.in"
+
         ));
 
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
